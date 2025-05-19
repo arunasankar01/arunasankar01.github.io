1edda85efbd4104e43a86db6c9bf60a250bb67c8
arunasankar01 <arunasankar01@gmail.com>
2025-05-19 12:20:30 -0400

    config changes

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/arunasankar01.github.io/',
+  base: '/',
   server: {
     open: true
   },
